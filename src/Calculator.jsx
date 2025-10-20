4c0a1db0ef013defbea83c59d3a74c27fc74ff4a
edilberto hernnadez <edilbertoh369@gmail.com>
2025-10-20 12:11:07 -0600

    Fixt lint errors

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,24 +1,24 @@
-import { useState } from "react";
-import { evaluate } from "mathjs";
+import { useState } from 'react'
+import { evaluate } from 'mathjs'
 
-export const operations = ["+", "-", "*", "/"];
-const equalSign = "=";
+export const operations = ['+', '-', '*', '/']
+const equalSign = '='
 
-export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+export const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]]
 
 export const Calculator = () => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
 
-  const createHandleClick = (op) => () => setValue(value.concat(op));
+  const createHandleClick = (op) => () => setValue(value.concat(op))
 
   return (
     <div>
       <h1>Calculator</h1>
-      
+
       <input value={value} readOnly />
-      <div role="grid">
+      <div role='grid'>
         {rows.map((row, idx) => (
-          <div key={idx} role="row">
+          <div key={idx} role='row'>
             {row.map((number) => (
               <button onClick={createHandleClick(number)} key={number}>
                 {number}
@@ -36,5 +36,5 @@ export const Calculator = () => {
         <button onClick={() => setValue(evaluate(value))}>{equalSign}</button>
       </div>
     </div>
-  );
-};
+  )
+}
